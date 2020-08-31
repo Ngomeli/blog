38d073191890729299b79edf6cf46345d7265692
Ngomeli <joshuangomeli@gmail.com>
2020-08-31 11:35:29 +0300

    added semantic ui

--- main.js
+++ main.js
@@ -30,6 +30,7 @@ var Blog = mongoose.model("Blog", blogSchema);
 app.get("/", function(req, res) {
     res.redirect("/blogs");
 });
+// INDEX ROUTE
 app.get("/blogs", function(req, res) {
     Blog.find({}, function(err, blogs) {
         if (err) {
@@ -38,7 +39,45 @@ app.get("/blogs", function(req, res) {
             res.render("index", { blogs: blogs });
         }
     });
-
+});
+//NEW ROUTE
+app.get("/blogs/new", function(req, res) {
+    res.render("new");
+});
+//CREATE ROUTE
+app.post("/blogs", function(req, res) {
+    //Create blog
+    Blog.create(req.body.blog, function(err, newBlog) {
+        if (err) {
+            res.render("new");
+        } else {
+            //then redirect to the index
+            res.redirect("/blogs");
+        }
+    });
+});
+//SHOW ROUTE
+app.get("/blogs/:id", function(req, res) {
+    //find the blog with provided ID
+    Blog.findById(req.params.id, function(err, foundBlog) {
+        if (err) {
+            res.redirect("/blogs");
+        } else {
+            //render show template with that blog
+            res.render("show", { blog: foundBlog });
+        }
+    });
+});
+//EDIT ROUTE
+app.get("/blogs/:id/edit", function(req, res) {
+    Blog.findById(req.params.id, function(err, foundBlog) {
+        if (err) {
+            res.redirect("/blogs");
+        } else {
+            //render show template with that blog
+            res.render("edit", { blog: foundBlog });
+        }
+    });
 });
 
 
